5931076f96f60e5eceb3a25039f14fa13702b16b
Brahim Bahaida <bbahieda@gmail.com>
2018-07-14 19:20:44 +0000

    refactor(Home): add carousel to the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import { Carousel, Grid, Row, Col, Image, Button } from 'react-bootstrap';
 import './Home.css';
 export default class Home extends Component {
 
     render(){
         return(
             <Grid>
-                <Jumbotron>
-                    <h2>Welcome to bbahaida.com</h2>
-                    <p>This is a website developed with React, React-Router, React-Bootstrap</p>
-                    <Link to="/about">
-                        <Button bsStyle="primary">
-                            About us
-                        </Button>
-                    </Link>
-                </Jumbotron>
+                <Carousel>
+                    <Carousel.Item className="" >
+                        <Image src="assets/cover-1.jpg" />
+                        <Carousel.Caption>
+                            <h2>Welcome to bbahaida.com</h2>
+                            <p>This is a website developed with React, React-Router, React-Bootstrap</p>
+                            <Link to="/about">
+                                <Button bsStyle="primary">
+                                    About us
+                                </Button>
+                            </Link>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Image src="assets/cover-2.jpg" />
+                        <Carousel.Caption>
+                            <h2>We are a team</h2>
+                            <p>This is a website developed with React, React-Router, React-Bootstrap</p>
+                            <Link to="/news">
+                                <Button bsStyle="primary">
+                                    News
+                                </Button>
+                            </Link>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    
+                </Carousel>
                 <Row className="show-grid text-center" >
                     <Col xs={12} sm={4} className="person-wrapper">
                         <Image src="assets/people-1.jpg" circle className="profile-pic" />
